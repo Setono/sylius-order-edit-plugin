76e13f3b48f26f2768bf5b8605468ef0d048fa6c
Mateusz Zalewski <zaleslaw@gmail.com>
2024-06-27 10:52:06 +0200

    Add delete button to new order item

--- src/Resources/public/js/order-edit.js
+++ src/Resources/public/js/order-edit.js
@@ -1,24 +1,32 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    document.querySelectorAll('button.delete-order-item').forEach((button) => {
-        button.addEventListener('click', (event) => {
-            const orderLineId = event.currentTarget.closest('tr').remove();
-        });
+document.querySelectorAll('button.delete-order-item').forEach((button) => {
+    button.addEventListener('click', (event) => {
+        var row = event.currentTarget.parentElement.parentElement;
+        row.nextElementSibling.remove();
+        row.remove();
     });
+});
 
-    document.querySelector('button.add-order-item').addEventListener('click', (event) => {
-        const orderItemTable = event.currentTarget.closest('table');
+document.querySelector('button.add-order-item').addEventListener('click', (event) => {
+    const orderItemTable = event.currentTarget.closest('table');
 
-        const html = orderItemTable
-            .dataset
-            .prototype
-            .replace(
-                /__name__/g,
-                orderItemTable.dataset.index
-            )
-        ;
+    const html = orderItemTable
+        .dataset
+        .prototype
+        .replace(
+            /__name__/g,
+            orderItemTable.dataset.index
+        )
+    ;
 
-        orderItemTable.querySelector('tbody').insertAdjacentHTML('beforeend', html)
+    orderItemTable.querySelector('tbody').insertAdjacentHTML('beforeend', html)
 
-        orderItemTable.dataset.index++;
+    orderItemTable.dataset.index++;
+
+    var rows = orderItemTable.querySelectorAll('form[name="sylius_order"] tbody tr');
+    var lastItemRowDeleteButton = rows[rows.length - 2].querySelector('button.delete-order-item');
+    lastItemRowDeleteButton.addEventListener('click', (event) => {
+        var row = event.currentTarget.parentElement.parentElement;
+        row.nextElementSibling.remove();
+        row.remove();
     });
 });
